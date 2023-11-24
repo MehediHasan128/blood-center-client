0cd6e79510628c35ade5ac416cc143cea6c752d0
MehediHasan128 <mehedihasan12926@gmail.com>
2023-11-24 18:22:52 +0530

    install react hooi form

--- src/Components/Pages/Authentication/Registration/Registration.jsx
+++ src/Components/Pages/Authentication/Registration/Registration.jsx
@@ -4,11 +4,14 @@ import { MdEmail } from "react-icons/md";
 import { TbPasswordFingerprint } from "react-icons/tb";
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { useForm } from "react-hook-form";
 
 const Registration = () => {
 
     const axiosPublic = useAxiosPublic();
     const [districts, setDistricts] = useState([]);
+    const [upazila, setUpazila] = useState([]);
+    const {register, handleSubmit} = useForm();
 
     useEffect(() =>{
         axiosPublic.get('/districts')
@@ -19,6 +22,22 @@ const Registration = () => {
 
     const handelFindUpazila = (e) =>{
         const districtName = e.target.value;
+        axiosPublic.get(`/upazila/${districtName}`)
+        .then(res =>{
+          setUpazila(res.data);
+        })
+    }
+
+    const onSubmit = (data) =>{
+      const donerName = data.name;
+      const donerEmail = data.email;
+      const blodGroup = data.bloodGroup;
+      const district = data.district;
+      const upazila = data.upazila;
+      const password = data.password;
+      const checkPassword = data.checkPassword;
+
+      const image = data.image[0].name;
     }
 
   return (
@@ -29,13 +48,14 @@ const Registration = () => {
         </div>
         <div className="w-[50%] bg-slate-50 dark:bg-zinc-700 py-14">
           <h1 className="text-4xl text-center dark:text-white">Register as a doner</h1>
-          <form className="w-[80%] mx-auto mt-8 space-y-5">
+          <form onSubmit={handleSubmit(onSubmit)} className="w-[80%] mx-auto mt-8 space-y-5">
             <div className="px-5 py-2 flex items-center gap-3 border rounded-md">
               <FaUser className="text-2xl text-gray-700 dark:text-white" />
               <input
                 className="bg-slate-50 px-5 py-2 w-full border-l-2 focus:outline-none dark:bg-zinc-700"
                 type="text"
                 placeholder="Enter your name"
+                {...register('name')}
               />
             </div>
             <div className="px-5 py-2 flex items-center gap-3 border rounded-md">
@@ -44,6 +64,7 @@ const Registration = () => {
                 className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full border-l-2 focus:outline-none"
                 type="email"
                 placeholder="Enter your email"
+                {...register('email')}
               />
             </div>
 
@@ -54,6 +75,7 @@ const Registration = () => {
                   className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full border-l-2 focus:outline-none"
                   type="password"
                   placeholder="Enter your password"
+                  {...register('password')}
                 />
               </div>
               <div className="px-5 py-2 flex items-center gap-3 border rounded-md">
@@ -62,12 +84,15 @@ const Registration = () => {
                   className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full border-l-2 focus:outline-none"
                   type="password"
                   placeholder="Enter possword again"
+                  {...register('checkPassword')}
                 />
               </div>
             </div>
 
             <div className="px-5 py-2 flex items-center gap-3 border rounded-md">
-              <select className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none">
+              <select className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none"
+              {...register('bloodGroup')}
+              >
                 <option>--- Blood Group ---</option>
                 <option value="A+">A+</option>
                 <option value="A-">A-</option>
@@ -82,7 +107,11 @@ const Registration = () => {
 
             <div className="flex gap-5">
             <div className="px-5 py-2 flex items-center gap-3 border w-full rounded-md">
-              <select onChange={handelFindUpazila} className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none">
+              <select className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none"
+              {...register('district', {
+                onChange: (e) => {handelFindUpazila(e)}
+              })}
+              >
                 <option>--- Select District ---</option>
                 {
                     districts.map(district => <option key={district._id} value={district.name}>{district.name}</option>)
@@ -90,22 +119,19 @@ const Registration = () => {
               </select>
             </div>
             <div className="px-5 py-2 flex items-center gap-3 border w-full rounded-md">
-              <select className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none">
+              <select className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none"
+              {...register('upazila')}
+              >
                 <option>--- Select Upazila ---</option>
-                <option value="A+">A+</option>
-                <option value="A-">A-</option>
-                <option value="B+">B+</option>
-                <option value="B-">B-</option>
-                <option value="AB+">AB+</option>
-                <option value="AB-">AB-</option>
-                <option value="O+">O+</option>
-                <option value="O-">O-</option>
+                {
+                  upazila.map(zila => <option key={zila._id} value={zila.name}>{zila.name}</option>)
+                }
               </select>
             </div>
             </div>
 
             <div className="px-5 py-2 flex items-center gap-3 border rounded-md">
-                <input className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none" type="file" id="upload" />
+                <input className="bg-slate-50 dark:bg-zinc-700 px-5 py-2 w-full focus:outline-none" type="file" {...register('image')} />
             </div>
             <div>
                 <input className="bg-red-700 hover:bg-red-800 duration-700 px-5 py-2 text-white rounded-md cursor-pointer" type="submit" value="Register" />
