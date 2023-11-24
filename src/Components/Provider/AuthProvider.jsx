4539be18aa5ba0ea5e02a372ca8864741747e88f
MehediHasan128 <mehedihasan12926@gmail.com>
2023-11-24 20:16:21 +0530

    add authProvider page set the all funtionality in authentication

---
+++ src/Components/Provider/AuthProvider.jsx
@@ -0,0 +1,61 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
+import auth from "../Firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true); 
+
+
+
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const setUserProfile = (user, name, photo) =>{
+        setLoading(true);
+        return updateProfile(user, {
+            displayName: name,
+            photoURL: photo
+        })
+    }
+
+
+    const userLogout = () =>{
+        setLoading(true);
+        return signOut(auth)
+    }
+
+
+    useEffect(() =>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () =>{
+            unSubscribe();
+        }
+    },[])
+
+
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        setUserProfile,
+        userLogout
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
